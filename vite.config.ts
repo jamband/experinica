14f9a534d2be0702cf9a1afec1494823a41dd671
Tomoki Morita <tmsongbooks215@gmail.com>
2022-09-16 20:43:04 +0900

    change module imports order

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@
 /// <reference types="vite/client" />
 
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
 import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   resolve: {
