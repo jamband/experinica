88a26515572bdc3dc663f23a13d57fc033ff3d79
Tomoki Morita <tmsongbooks215@gmail.com>
2022-12-08 22:30:42 +0900

    use gap instead of margin in section divider component

--- src/components/section-divider/component.tsx
+++ src/components/section-divider/component.tsx
@@ -2,15 +2,17 @@ import type { _Props } from "./types";
 
 export const Component: React.FC<_Props> = (props) => (
   <div
-    className={`${props.className || ""} flex items-center justify-center`}
+    className={`flex items-center justify-center gap-x-1 ${
+      props.className || ""
+    }`}
     role="separator"
   >
-    <span className="mr-1 h-1 w-1 rounded-[1px] bg-yellow-500" />
-    <span className="mr-1 h-3 w-1 rounded-[1px] bg-yellow-600" />
-    <span className="mr-1 h-6 w-1 rounded-[1px] bg-yellow-800" />
-    <span className="mr-1 h-4 w-1 rounded-[1px] bg-yellow-700" />
-    <span className="mr-1 h-7 w-1 rounded-[1px] bg-yellow-500" />
-    <span className="mr-1 h-3 w-1 rounded-[1px] bg-yellow-600" />
+    <span className="h-1 w-1 rounded-[1px] bg-yellow-500" />
+    <span className="h-3 w-1 rounded-[1px] bg-yellow-600" />
+    <span className="h-6 w-1 rounded-[1px] bg-yellow-800" />
+    <span className="h-4 w-1 rounded-[1px] bg-yellow-700" />
+    <span className="h-7 w-1 rounded-[1px] bg-yellow-500" />
+    <span className="h-3 w-1 rounded-[1px] bg-yellow-600" />
     <span className="h-1 w-1 rounded-[1px] bg-yellow-400" />
   </div>
 );
