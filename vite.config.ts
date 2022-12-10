750346c7ac451134fb61f18ed22f98fbe45cab3f
Tomoki Morita <tmsongbooks215@gmail.com>
2022-12-10 23:07:57 +0900

    use plugin-react-swc instead of plugin-react

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
