cffd6cdf18201011e5832d828fb79dcb42bfe682
Tomoki Morita <tmsongbooks215@gmail.com>
2023-11-29 23:20:57 +0900

    fix some styles

--- src/layouts/loading/component.tsx
+++ src/layouts/loading/component.tsx
@@ -1,14 +1,16 @@
+import styles from "./styles.module.css";
+
 export const Component: React.FC = () => (
-  <div className="flex h-[70vh] items-center justify-center">
+  <div className={styles.container}>
     <svg
-      className="h-7 w-7 animate-spin text-yellow-500"
+      className={styles.svg}
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
       role="status"
     >
       <circle
-        className="opacity-25"
+        className={styles.circle}
         cx="12"
         cy="12"
         r="10"
@@ -16,7 +18,7 @@ export const Component: React.FC = () => (
         strokeWidth="4"
       />
       <path
-        className="opacity-75"
+        className={styles.path}
         fill="currentColor"
         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
       />
