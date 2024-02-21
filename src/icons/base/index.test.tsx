5e8b20aa671316c7b68a57dd7319920df5bea9ef
Tomoki Morita <tmsongbooks215@gmail.com>
2024-02-21 22:58:45 +0900

    fix base icon component test

--- src/icons/base/index.test.tsx
+++ src/icons/base/index.test.tsx
@@ -4,14 +4,14 @@ import { Icon } from ".";
 
 test("", () => {
   render(
-    <Icon className="h-4 w-4" viewBox="0 0 20 20">
+    <Icon className="foo" viewBox="0 0 20 20">
       icon
     </Icon>,
   );
 
   const imageElement = screen.getByText("icon");
   expect(imageElement).toHaveAttribute("viewBox", "0 0 20 20");
-  expect(imageElement).toHaveClass("inline-block h-4 w-4", { exact: true });
+  expect(imageElement).toHaveClass("foo", { exact: true });
   expect(imageElement).not.toHaveAccessibleName("img");
   expect(imageElement).toHaveAttribute("role", "img");
   expect(imageElement).toHaveAttribute("aria-hidden", "true");
