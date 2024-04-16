96a3a99904e59a079a3cb77269823aeda1c9daf5
Tomoki Morita <tmsongbooks215@gmail.com>
2024-04-16 16:45:10 +0900

    use external props type

--- src/contexts/tape.tsx
+++ src/contexts/tape.tsx
@@ -1,13 +1,10 @@
 import type { Dispatch, State } from "@/reducers/tape";
 import { initialState, reducer } from "@/reducers/tape";
 import { createContext, useReducer } from "react";
+import type { Props } from "./types";
 
 export const StateContext = createContext<State>({} as State);
 export const DispatchContext = createContext<Dispatch>({} as Dispatch);
-
-export type Props = {
-  children: React.ReactNode;
-};
 
 export const TapeProvider: React.FC<Props> = (props) => {
   const [state, dispatch] = useReducer(reducer, initialState);
--- src/contexts/track.tsx
+++ src/contexts/track.tsx
@@ -1,13 +1,10 @@
 import type { Dispatch, State } from "@/reducers/track";
 import { initialState, reducer } from "@/reducers/track";
 import { createContext, useReducer } from "react";
+import type { Props } from "./types";
 
 export const StateContext = createContext<State>({} as State);
 export const DispatchContext = createContext<Dispatch>({} as Dispatch);
-
-export type Props = {
-  children: React.ReactNode;
-};
 
 export const TrackProvider: React.FC<Props> = (props) => {
   const [state, dispatch] = useReducer(reducer, initialState);
