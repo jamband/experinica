5a6014da3ce5b931db15b80809ba19116890b38f
Tomoki Morita <tmsongbooks215@gmail.com>
2024-10-11 08:39:25 +0900

    use jsdom instead of happy-dom

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: "happy-dom",
+    environment: "jsdom",
     setupFiles: "../vitest.setup.ts",
   },
 });
