33141e91329b83272a598c606960a48d04658d8a
Tomoki Morita <tmsongbooks215@gmail.com>
2026-03-20 14:07:51 +0900

    remove unnecessary some comments

--- src/layouts/loading/component.tsx
+++ src/layouts/loading/component.tsx
@@ -2,7 +2,6 @@ import styles from "./styles.module.css";
 
 export const Component: React.FC = () => (
   <div className={styles.container}>
-    {/** biome-ignore lint/a11y/useSemanticElements: reason */}
     <svg
       className={styles.svg}
       xmlns="http://www.w3.org/2000/svg"
